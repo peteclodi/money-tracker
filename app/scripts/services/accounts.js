aabf260d5c224aeed67c99aeba467c55ac37d57d
peteclodi <pete.clodi@gmail.com>
2014-07-15 15:03:44 -0500

    minor changes

--- app/scripts/services/accounts.js
+++ app/scripts/services/accounts.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('moneyTrackerApp')
-    .factory('Accounts', function($resource) {
-        return new $resource('http://peteclodi.com/code/money-tracker/test_data/accounts.json');
+    .factory('Accounts', function ($resource) {
+        return new $resource('/data/accounts.json');
     });
