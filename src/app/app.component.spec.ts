759d0182720ccafffee2c0e7717caacd1b8825ec
Pete Clodi <pete.clodi@gmail.com>
2016-08-21 21:56:26 -0500

    Fix Unit Tests :shipit:

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,8 +13,8 @@ describe('App: MoneyTracker', () => {
       expect(app).toBeTruthy();
     }));
 
-  it('should have as title \'app works!\'',
+  it('should have as title \'Money Tracker works!\'',
     inject([AppComponent], (app: AppComponent) => {
-      expect(app.title).toEqual('app works!');
+      expect(app.title).toEqual('Money Tracker works!');
     }));
 });
